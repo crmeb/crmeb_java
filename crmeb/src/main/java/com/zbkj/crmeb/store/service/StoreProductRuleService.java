b9b49f6e85a5e617e210743c0ca685bc05bca69d
张乐 <419641032@qq.com>
2020-08-24 15:25:23 +0800

    1、优化产品属性、规格唯一性 2、修复佣金获取数值计算不争取的问题

--- crmeb/src/main/java/com/zbkj/crmeb/store/service/StoreProductRuleService.java
+++ crmeb/src/main/java/com/zbkj/crmeb/store/service/StoreProductRuleService.java
@@ -3,6 +3,7 @@ package com.zbkj.crmeb.store.service;
 import com.common.PageParamRequest;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.zbkj.crmeb.store.model.StoreProductRule;
+import com.zbkj.crmeb.store.request.StoreProductRuleRequest;
 import com.zbkj.crmeb.store.request.StoreProductRuleSearchRequest;
 
 import java.util.List;
@@ -15,4 +16,11 @@ import java.util.List;
 public interface StoreProductRuleService extends IService<StoreProductRule> {
 
     List<StoreProductRule> getList(StoreProductRuleSearchRequest request, PageParamRequest pageParamRequest);
+
+    /**
+     * 新增商品规格
+     * @param storeProductRuleRequest 规格参数
+     * @return 新增结果
+     */
+    boolean save(StoreProductRuleRequest storeProductRuleRequest);
 }
