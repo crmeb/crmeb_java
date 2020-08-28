92d288ef2122dc474a43cc0b4cb13df6ea3f2f40
张乐 <419641032@qq.com>
2020-08-28 15:55:13 +0800

    1、新增恢复回收站商品功能 2、短信记录显示内容

--- crmeb/src/main/java/com/zbkj/crmeb/task/sms/AsyncSmsSendResult.java
+++ crmeb/src/main/java/com/zbkj/crmeb/task/sms/AsyncSmsSendResult.java
@@ -18,26 +18,26 @@ import org.springframework.stereotype.Component;
  * @Date 2020/8/18
  * @Created by stivepeim
  */
-//@Component
-//@Configuration //读取配置
-//@EnableScheduling // 2.开启定时任务
+@Component
+@Configuration //读取配置
+@EnableScheduling // 2.开启定时任务
 public class AsyncSmsSendResult {
-//    //日志
-//    private static final Logger logger = LoggerFactory.getLogger(AsyncSmsSendResult.class);
-//
-//    @Autowired
-//    private SmsRecordService smsRecordsService;
-//
-//    @Scheduled(fixedDelay = 1000 * 10L) // todo 后面更改为 一分钟同步一次数据
-//    public void init(){
-//        logger.info("---AsyncSmsResult task------produce Data with fixed rate task: Execution Time - {}", DateUtil.nowDate());
-//        try {
-//            smsRecordsService.consumeSmsStatus();
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//            logger.error("AsyncSmsSend.task" + " | msg : " + e.getMessage());
-//        }
-//
-//    }
+    //日志
+    private static final Logger logger = LoggerFactory.getLogger(AsyncSmsSendResult.class);
+
+    @Autowired
+    private SmsRecordService smsRecordsService;
+
+    @Scheduled(fixedDelay = 1000 * 60L) // todo 后面更改为 一分钟同步一次数据
+    public void init(){
+        logger.info("---AsyncSmsResult task------produce Data with fixed rate task: Execution Time - {}", DateUtil.nowDate());
+        try {
+            smsRecordsService.consumeSmsStatus();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            logger.error("AsyncSmsSend.task" + " | msg : " + e.getMessage());
+        }
+
+    }
 }
