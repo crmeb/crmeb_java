33549e54898e2a4dd51cd7ed352648e72b4c03ac
guaishoudemao <598705433@qq.com>
2021-02-06 17:11:40 +0800

    更新代码

---
+++ admin/src/views/maintain/picture/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="divBox">
+    <el-card class="box-card">
+      <upload-index :pictureType="pictureType"></upload-index>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  import UploadIndex from '@/components/uploadPicture/index.vue'
+
+  export default {
+    name: "index",
+    data(){
+      return {
+        pictureType: 'maintain'
+      }
+    },
+    components: {UploadIndex},
+  }
+</script>
+
+<style scoped>
+
+</style>
