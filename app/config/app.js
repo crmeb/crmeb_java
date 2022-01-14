ce4ec543a52898e9f6af369a361480dca42024db
hejinfu1026 <18537720348@163.com>
2022-01-14 09:50:15 +0800

    移动端提前缓存城市数据，避免编辑地址时等待请求

--- app/config/app.js
+++ app/config/app.js
@@ -1,12 +1,12 @@
 
-let domain = 'https://xxx.xxx'
+let domain = 'xxx.xxx'
 
 module.exports = {
 	// 请求域名 格式： https://您的域名
 	// #ifdef MP
 		HTTP_REQUEST_URL: domain,
 	// #endif
-	HTTP_ADMIN_URL:'https://xxx.xxx.xxx', //PC后台的API请求地址，上传图片用
+	HTTP_ADMIN_URL:'xxx.xxx.xxx', //PC后台的API请求地址，上传图片用
 	// #ifdef H5
 		//H5接口是浏览器地址
 		// HTTP_REQUEST_URL: window.location.protocol+"//"+window.location.host,
