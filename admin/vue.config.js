2533f0cdac1649fafbda87b3f6e7fadead269882
337031187 <stivepeim@outlook.com>
2022-02-07 11:33:01 +0800

    清理 mock信息 环境文件更新

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -36,8 +36,7 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
-    },
-    before: require('./mock/mock-server.js')
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
