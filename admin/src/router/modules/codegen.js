79c77c4dd7b34cb9691a0f617dd98b21a3637a5a
337031187 <stivepeim@outlook.com>
2022-09-08 15:39:41 +0800

    代码生成 web界面

--- admin/src/router/modules/codegen.js
+++ admin/src/router/modules/codegen.js
@@ -11,45 +11,20 @@
 import Layout from '@/layout'
 
 const storeRouter = {
-    path: '/store',
+    path: '/codegen',
     component: Layout,
-    redirect: '/store/index',
+    redirect: '/codegen/codegenList',
     name: 'Store',
     meta: {
-      title: '商品',
+      title: '代码生成',
       icon: 'clipboard'
     },
     children: [
       {
-        path: 'index',
-        component: () => import('@/views/store/index'),
+        path: 'codegenList',
+        component: () => import('@/views/codegen/codegenList'),
         name: 'StoreIndex',
-        meta: { title: '商品管理', icon: '' }
-      },
-      {
-        path: 'sort',
-        component: () => import('@/views/store/sort/index'),
-        name: 'Sort',
-        meta: { title: '商品分类', icon: '' }
-      },
-      {
-        path: 'attr',
-        component: () => import('@/views/store/storeAttr/index'),
-        name: 'SortAttr',
-        meta: { title: '商品规格', icon: '' }
-      },
-      {
-        path: 'comment',
-        component: () => import('@/views/store/storeComment/index'),
-        name: 'StoreComment',
-        meta: { title: '商品评论', icon: '' }
-      },
-      {
-        path: 'list/creatProduct/:id?/:isDisabled?',
-        component: () => import('@/views/store/creatStore/index'),
-        name: 'SortCreat',
-        meta: { title: '商品添加', noCache: true, activeMenu: `/store/index` },
-        hidden: true
+        meta: { title: '待生成列表', icon: '' }
       }
     ]
   }
